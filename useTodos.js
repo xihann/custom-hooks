b97b65a6bb31f7c126c20c7285d1b6508a4187d6
Axel <toshiyuki93@gmail.com>
2025-02-24 13:39:30 -0600

    Primer commit

---
+++ useTodos.js
@@ -0,0 +1,50 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || []
+}
+
+export const useTodos = () => {
+
+
+    const [todos, dispatchTodo] = useReducer(todoReducer, [], init);
+
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos])
+
+
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+        dispatchTodo(action);
+    }
+
+    const handleDeleteTodo = (id) => {
+        dispatchTodo({
+            type: '[TODO] Remove Todo',
+            payload: id
+        })
+    }
+
+    const handleToggleTodo = (id) => {
+        dispatchTodo({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        });
+    }
+
+    return {
+        todos,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(todo => !todo.done).length,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+    }
+
+}
